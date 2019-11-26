cea3f4c92e6cb5716df52163b05770d2b32cb278
Cezary Krawczyk <cezary.k.krawczyk@gmail.com>
2019-11-26 15:56:32 +0100

    init project

---
+++ src/main/java/pl/ug/virtualofficebackend/VirtualOfficeBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.ug.virtualofficebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualOfficeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualOfficeBackendApplication.class, args);
+    }
+
+}
