30370ce73010e23975ad02f8c7e6177ca0239a1d
Paweł <apawell@o2.pl>
2020-01-07 18:45:44 +0100

    lil fix

--- src/main/java/pl/ug/virtualofficebackend/domain/user/entity/User.java
+++ src/main/java/pl/ug/virtualofficebackend/domain/user/entity/User.java
@@ -3,7 +3,6 @@ package pl.ug.virtualofficebackend.domain.user.entity;
 import pl.ug.virtualofficebackend.domain.entity.Workstation;
 
 import javax.persistence.*;
-import java.sql.Date;
 import java.util.UUID;
 
 @Entity(name = "office_user")
@@ -16,16 +15,6 @@ public class User {
     private String name;
     private String secondName;
     private String surname;
-
-    private Date birthDate;
-    private String parentNames;
-    private String nationality;
-
-    private String country;
-    private String city;
-    private String region;
-    private String postalCode;
-    private String address;
 
     @OneToOne(mappedBy = "user")
     private Workstation workstation;
@@ -69,70 +58,6 @@ public class User {
 
     public void setSurname(String surname) {
         this.surname = surname;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getParentNames() {
-        return parentNames;
-    }
-
-    public void setParentNames(String parentNames) {
-        this.parentNames = parentNames;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public Workstation getWorkstation() {
