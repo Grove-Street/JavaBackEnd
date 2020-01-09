882fd934688879cc85164f4700282afde06a318f
pkrause <apawell@o2.pl>
2020-01-09 11:08:15 +0100

    fix repo

--- src/main/java/pl/ug/virtualofficebackend/domain/user/internal/UserRepository.java
+++ src/main/java/pl/ug/virtualofficebackend/domain/user/internal/UserRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.ug.virtualofficebackend.domain.user.entity.User;
 
-import java.util.UUID;
-
 @Repository
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends CrudRepository<User, Long> {
 }
