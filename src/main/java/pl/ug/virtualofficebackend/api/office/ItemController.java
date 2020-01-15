ec2538cb5b95cb6baaafc21fb1c847e21cedbe59
Paweł Krause <pawel.krause@ihsmarkit.com>
2020-01-15 08:30:28 +0100

    add post mapping

--- src/main/java/pl/ug/virtualofficebackend/api/office/ItemController.java
+++ src/main/java/pl/ug/virtualofficebackend/api/office/ItemController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.ug.virtualofficebackend.domain.item.boundary.ItemService;
@@ -37,6 +39,17 @@ public class ItemController {
         }
 
         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("api/item")
+    public ResponseEntity<Item> postWorkstation(RequestEntity<Item> requestItem) {
+        Item item = requestItem.getBody();
+
+        if(item != null && this.itemService.add(item)) {
+            return new ResponseEntity<>(item, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(item, HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/main/java/pl/ug/virtualofficebackend/api/user/UserController.java
+++ src/main/java/pl/ug/virtualofficebackend/api/user/UserController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.ug.virtualofficebackend.domain.user.boundary.UserService;
@@ -37,6 +39,17 @@ public class UserController {
         }
 
         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("api/user")
+    public ResponseEntity<User> postWorkstation(RequestEntity<User> requestUser) {
+        User user = requestUser.getBody();
+
+        if(user != null && this.userService.add(user)) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
     }
 
 }
