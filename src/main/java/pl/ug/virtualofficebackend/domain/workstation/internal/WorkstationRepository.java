9e828061efbb3c9d240c0ed7f3aa89916b1d1946
Paweł Krause <apawell@o2.pl>
2020-01-15 08:01:51 +0100

    add more controllers

--- src/main/java/pl/ug/virtualofficebackend/domain/workstation/internal/WorkstationRepository.java
+++ src/main/java/pl/ug/virtualofficebackend/domain/workstation/internal/WorkstationRepository.java
@@ -1,8 +1,8 @@
 package pl.ug.virtualofficebackend.domain.workstation.internal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import pl.ug.virtualofficebackend.domain.workstation.entity.Workstation;
 
 @Repository
