7da510d1f81212a5a36d83791331612176c54d61
mzmeczerowski <mzmeczerowski@sigma.ug.edu.pl>
2020-01-24 21:27:47 +0100

    creating in proces...

--- src/main/java/pl/ug/virtualofficebackend/api/web/AdminController.java
+++ src/main/java/pl/ug/virtualofficebackend/api/web/AdminController.java
@@ -3,49 +3,38 @@ package pl.ug.virtualofficebackend.api.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import pl.ug.virtualofficebackend.domain.decoration.entity.Decoration;
 import pl.ug.virtualofficebackend.domain.office.entity.Office;
-import pl.ug.virtualofficebackend.domain.user.boundary.UserService;
+import pl.ug.virtualofficebackend.domain.office.internal.OfficeServiceImpl;
+import pl.ug.virtualofficebackend.domain.room.entity.Room;
 import pl.ug.virtualofficebackend.domain.user.entity.User;
-import pl.ug.virtualofficebackend.domain.user.internal.UserServiceImpl;
+import pl.ug.virtualofficebackend.domain.workstation.entity.Workstation;
+import pl.ug.virtualofficebackend.domain.workstation.internal.WorkstationServiceImpl;
 
-import javax.validation.Valid;
+import java.util.ArrayList;
 
 @Controller
 public class AdminController {
 
     @Autowired
-    UserServiceImpl userService;
+    OfficeServiceImpl officeService;
+
 
     @GetMapping("/admin")
-    public String panel(Model model) {
-        model.addAttribute("users", userService.getAll());
-        model.addAttribute("newUser", new User());
+    public String start(Model model) {
+
+        Office newOffice = new Office();
+        newOffice.setDecorations(new ArrayList<Decoration>());
+        newOffice.setRooms(new ArrayList<Room>());
+        newOffice.setWorkstations(new ArrayList<Workstation>());
+
+
+        User newManager = new User();
+        newManager.setOffice(newOffice);
+
+        model.addAttribute("newManager", newManager);
+
         return "admin";
-    }
-
-    @PostMapping("/admin/add")
-    public String panel(@Valid User user, Errors errors, Model model) {
-        if (errors.hasErrors()) {
-            model.addAttribute("users", userService.getAll());
-            model.addAttribute("newUser", user);
-            return "admin";
-        }
-
-        User userToAdd = new User();
-        userToAdd.setName(user.getName());
-        userToAdd.setSecondName(user.getSecondName());
-        userToAdd.setSurname(user.getSurname());
-        userToAdd.setEmail(user.getEmail());
-        userToAdd.setCountry(user.getCountry());
-        userToAdd.setUsername(user.getUsername());
-        userToAdd.setWorkstation(user.getWorkstation());
-        userToAdd.setOffice(new Office()); // loggedManager.getOffice()
-        userService.save(userToAdd);
-
-        return "redirect:/admin";
     }
 }
---
+++ src/main/java/pl/ug/virtualofficebackend/api/web/ManagerController.java
@@ -0,0 +1,61 @@
+package pl.ug.virtualofficebackend.api.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.ug.virtualofficebackend.domain.office.entity.Office;
+import pl.ug.virtualofficebackend.domain.user.boundary.UserService;
+import pl.ug.virtualofficebackend.domain.user.entity.User;
+import pl.ug.virtualofficebackend.domain.user.internal.UserServiceImpl;
+import pl.ug.virtualofficebackend.domain.workstation.entity.Workstation;
+
+import javax.validation.Valid;
+
+@Controller
+public class ManagerController {
+
+    @Autowired
+    UserServiceImpl userService;
+
+    User manager = new User(); // 100 - id zalogowanego managera
+    // Potrzebne dane zalogowanego user'a jako manager od Spring Security
+
+    @GetMapping("/manager")
+    public String panel(Model model) {
+        model.addAttribute("users", userService.getAll());
+        model.addAttribute("newUser", new User());
+
+        model.addAttribute("manager", manager);
+        return "manager";
+    }
+
+    @PostMapping("/manager/add")
+    public String panel(@Valid User user, Errors errors, Model model) {
+
+        System.out.println("###\n" + user + "\n###");
+
+        if (errors.hasErrors()) {
+            model.addAttribute("users", userService.getAll());
+            model.addAttribute("newUser", user);
+            model.addAttribute("manager", manager);
+            return "manager";
+        }
+
+        User userToAdd = new User();
+        userToAdd.setName(user.getName());
+        userToAdd.setSecondName(user.getSecondName());
+        userToAdd.setSurname(user.getSurname());
+        userToAdd.setEmail(user.getEmail());
+        userToAdd.setCountry(user.getCountry());
+        userToAdd.setUsername(user.getUsername());
+        userToAdd.setWorkstation(user.getWorkstation());
+        userToAdd.setOffice(manager.getOffice()); // loggedManager.getOffice()
+        userService.save(userToAdd);
+
+        return "redirect:/manager";
+    }
+}
